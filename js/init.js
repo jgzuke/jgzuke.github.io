2295ce43d9fefcb6a35e43a8fc2d6aa712ed0aa9
jgzuke <jgzuke@gmail.com>
2015-06-23 01:07:43 -0400

    Overlay works perfectly, writing strings

--- js/init.js
+++ js/init.js
@@ -21,3 +21,26 @@ $('#fab-contact, #link-contact').click(function()
 		scrollTop: $('#page-contact').offset().top
 	}, 'slow');
 });
+
+$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function(e) {
+	resizeImageOverlays();
+});
+
+$(document).ready(function() {
+	resizeImageOverlays();
+});
+
+function resizeImageOverlays() {
+	var imHeight = $('#firstPicture').css('height');
+	var imWidth = $('#firstPicture').css('width');
+	$('.image-overlay-text').css('height', imHeight);
+	$('.image-overlay-text').css('width', imWidth);
+
+	var heightInt = parseInt(imHeight, "10");
+	var fontSizeScale = Math.pow(heightInt, 1); //constant to keep text scaling with size
+	var fontSizeTitle = Math.floor(fontSizeScale * 0.7) + '%';
+	var fontSizeBody = Math.floor(fontSizeScale*0.47) + '%';
+
+	$('.image-overlay-text-title').css('font-size', fontSizeTitle);
+	$('.image-overlay-text-main').css('font-size', fontSizeBody);
+}
