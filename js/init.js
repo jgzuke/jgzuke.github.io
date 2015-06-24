42887d0cee6984f44d6127dd978cd29b928b0d37
jgzuke <jgzuke@gmail.com>
2015-06-24 02:52:45 -0400

    set text size on scale, new icons

--- js/init.js
+++ js/init.js
@@ -31,11 +31,14 @@ $(document).ready(function() {
 });
 
 function resizeImageOverlays() {
+	// Sets overlays to same hieght and width as pictures
 	var imHeight = $('#firstPicture').css('height');
 	var imWidth = $('#firstPicture').css('width');
 	$('.image-overlay-text').css('height', imHeight);
 	$('.image-overlay-text').css('width', imWidth);
 
+
+	// Sets text size to fit well in overlay based on overlay dimensions
 	var heightInt = parseInt(imHeight, "10");
 	var fontSizeScale = Math.pow(heightInt, 1); //constant to keep text scaling with size
 	var fontSizeTitle = Math.floor(fontSizeScale * 0.65) + '%';
@@ -43,4 +46,15 @@ function resizeImageOverlays() {
 
 	$('.image-overlay-text-title').css('font-size', fontSizeTitle);
 	$('.image-overlay-text-main').css('font-size', fontSizeBody);
+
+
+	// Sets text to scale nicely with the size of the device screen (a bit smaller on phones)
+	var textScale = Math.pow($(window).width(), 0.48) / 30;
+
+	var landingTitle = Math.floor(textScale * 600) + '%';
+	var landingText = Math.floor(textScale * 150) + '%';
+	var normalTitle = Math.floor(textScale * 500) + '%';
+	$('#landing-title').css('font-size', landingTitle);
+	$('.landing-text').css('font-size', landingText);
+	$('.section-header').css('font-size', normalTitle);
 }
