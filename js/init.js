1ddb03fe82572811feaec529f74f85ec99102bdf
jgzuke <jgzuke@gmail.com>
2015-08-29 17:21:30 -0400

    init one page

--- js/init.js
+++ js/init.js
@@ -1,33 +1,5 @@
-$('.link-home').click(function() {
-	$('html, body').animate({
-		scrollTop: $('#page-home').offset().top
-	}, 'slow');
-});
-
-$('.link-projects').click(function() {
-	$('html, body').animate({
-		scrollTop: $('#page-projects').offset().top
-	}, 'slow');
-});
-
-$('.link-work').click(function() {
-	$('html, body').animate({
-		scrollTop: $('#page-work').offset().top
-	}, 'slow');
-});
-
 $('.link-contact').click(function() {
-	$('html, body').animate({		
-		scrollTop: $('#page-contact').offset().top		
-	}, 'slow');
-});
-
-$('.select-projects-next').click(function() {
-	showProjectSet(currentProjectSet + 1);
-});
-
-$('.select-projects-prev').click(function() {
-	showProjectSet(currentProjectSet - 1);
+	$('#contact-modal').openModal();
 });
 
 var currentProjectSet = 1;
@@ -110,8 +82,8 @@ function resizeImageOverlays() {
 	// Sets text size to fit well in overlay based on overlay dimensions
 	var heightInt = parseInt(imHeight, "10");
 	var fontSizeScale = Math.pow(heightInt, 1); //constant to keep text scaling with size
-	var fontSizeTitle = Math.floor(fontSizeScale * 0.65) + '%';
-	var fontSizeBody = Math.floor(fontSizeScale*0.46) + '%';
+	var fontSizeTitle = Math.floor(fontSizeScale * 0.55) + '%';
+	var fontSizeBody = Math.floor(fontSizeScale*0.26) + '%';
 
 	$('.image-overlay-text-title').css('font-size', fontSizeTitle);
 	$('.image-overlay-text-main').css('font-size', fontSizeBody);
@@ -120,10 +92,9 @@ function resizeImageOverlays() {
 	// Sets text to scale nicely with the size of the device screen (a bit smaller on phones)
 	var textScale = Math.pow($(window).width(), 0.48) / 30;
 
-	var landingTitle = Math.floor(textScale * 600) + '%';
-	var landingText = Math.floor(textScale * 180) + '%';
+	var landingTitle = Math.floor(textScale * 300) + '%';
+	var landingText = Math.floor(textScale * 110) + '%';
 	var normalTitle = Math.floor(textScale * 400) + '%';
 	$('#landing-title').css('font-size', landingTitle);
 	$('.landing-text').css('font-size', landingText);
-	$('.section-header').css('font-size', normalTitle);
 }
