71c2d9078836ad1b4397dd0ce82014e3b1112453
jgzuke <jgzuke@gmail.com>
2017-10-23 17:20:00 +0800

    html generated by clean script

--- js/init.js
+++ js/init.js
@@ -3,11 +3,12 @@ var currentPageName = '';
 
 function showPage(pageName) {
     if(currentPageName == pageName) return;
+    var oldPageName = currentPageName;
     $('#page-right').fadeOut(projectChangeAnimationTime);
     $('#navLinks h4').removeClass("active");
     $(`#${pageName}Link`).addClass("active");
     setTimeout(function () {
-        $(`#${currentPageName}`).hide();
+        $(`#${oldPageName}`).hide();
         $(`#${pageName}`).show();
         $('#page-right').fadeIn(projectChangeAnimationTime);
     }, projectChangeAnimationTime);
@@ -15,5 +16,8 @@ function showPage(pageName) {
 }
 
 $(document).ready(function() {
-    showPage('about');
+    currentPageName = 'about';
+    $('#experiences').hide();
+    $('#projects').hide();
+    $('#initiatives').hide();
 });
