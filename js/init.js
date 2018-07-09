45e9996322927eddbe4c7694d8e284ca1686c94e
jgzuke <jgzuke@gmail.com>
2018-07-08 18:47:44 -0700

    remove some jquery

--- js/init.js
+++ js/init.js
@@ -1,23 +1,30 @@
-var projectChangeAnimationTime = 150;
-var currentPageName = '';
+const PROJECT_CHANGE_ANIMATION_TIME = 150;
+const ABOUT = 'about';
+const EXPERIENCE = 'experience';
+const PROJECTS = 'projects';
+const INITIATIVES = 'initiatives';
+let currentPageName = ABOUT;
 
 function showPage(pageName) {
-    if(currentPageName == pageName) return;
-    var oldPageName = currentPageName;
-    $('#page-right').fadeOut(projectChangeAnimationTime);
-    $('#navLinks h4').removeClass("active");
-    $(`#${pageName}Link`).addClass("active");
+    if (currentPageName === pageName) {
+        return;
+    }
+    const oldPageName = currentPageName;
+    $('#page-right').fadeOut(PROJECT_CHANGE_ANIMATION_TIME);
+    document.getElementById(`${oldPageName}Link`).classList.remove("active");
+    document.getElementById(`${pageName}Link`).classList.add("active");
     setTimeout(function () {
-        $(`#${oldPageName}`).hide();
-        $(`#${pageName}`).show();
-        $('#page-right').fadeIn(projectChangeAnimationTime);
-    }, projectChangeAnimationTime);
+        const currentDisplay = pageName === ABOUT? 'flex' : 'block';
+        document.getElementById(oldPageName).style.display = 'none';
+        document.getElementById(pageName).style.display = currentDisplay;
+        $('#page-right').fadeIn(PROJECT_CHANGE_ANIMATION_TIME);
+    }, PROJECT_CHANGE_ANIMATION_TIME);
     currentPageName = pageName;
 }
 
 $(document).ready(function() {
-    currentPageName = 'about';
-    $('#experience').hide();
-    $('#projects').hide();
-    $('#initiatives').hide();
+    currentPageName = ABOUT;
+    document.getElementById(EXPERIENCE).style.display = 'none';
+    document.getElementById(PROJECTS).style.display = 'none';
+    document.getElementById(INITIATIVES).style.display = 'none';
 });
