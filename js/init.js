a0724dfe36219a3c1a5494e835283984d8c6654a
jgzuke <jgzuke@gmail.com>
2018-07-08 19:26:20 -0700

    remove jquery and use classnames over styles in js

--- js/init.js
+++ js/init.js
@@ -1,30 +1,18 @@
 const PROJECT_CHANGE_ANIMATION_TIME = 150;
-const ABOUT = 'about';
-const EXPERIENCE = 'experience';
-const PROJECTS = 'projects';
-const INITIATIVES = 'initiatives';
-let currentPageName = ABOUT;
+let currentPageName = 'about';
 
 function showPage(pageName) {
     if (currentPageName === pageName) {
         return;
     }
     const oldPageName = currentPageName;
-    $('#page-right').fadeOut(PROJECT_CHANGE_ANIMATION_TIME);
-    document.getElementById(`${oldPageName}Link`).classList.remove("active");
-    document.getElementById(`${pageName}Link`).classList.add("active");
+    document.getElementById('page-right').classList.add("faded");
+    document.getElementById(`${oldPageName}Link`).classList.remove("activeLink");
+    document.getElementById(`${pageName}Link`).classList.add("activeLink");
     setTimeout(function () {
-        const currentDisplay = pageName === ABOUT? 'flex' : 'block';
-        document.getElementById(oldPageName).style.display = 'none';
-        document.getElementById(pageName).style.display = currentDisplay;
-        $('#page-right').fadeIn(PROJECT_CHANGE_ANIMATION_TIME);
+        document.getElementById('page-right').classList.remove("faded");
+        document.getElementById(oldPageName).classList.add("hiddenPage");
+        document.getElementById(pageName).classList.remove("hiddenPage");
     }, PROJECT_CHANGE_ANIMATION_TIME);
     currentPageName = pageName;
 }
-
-$(document).ready(function() {
-    currentPageName = ABOUT;
-    document.getElementById(EXPERIENCE).style.display = 'none';
-    document.getElementById(PROJECTS).style.display = 'none';
-    document.getElementById(INITIATIVES).style.display = 'none';
-});
